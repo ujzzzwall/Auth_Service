e2a4e975d56bd8d685ab870eec9fe6a001cf24bf
Ujjwal singh <ujzzzwall@gmail.com>
2025-12-31 21:28:10 +0530

    First commit : Project skeleton setup done

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {PORT} = require('./config/serverConfig');
+const app = express();
+
+const prepareAndStartServer = ()=>{
+  app.listen(PORT , ()=>{
+    console.log(`server started at PORT : ${PORT}`)
+  })
+}
+
+prepareAndStartServer();
