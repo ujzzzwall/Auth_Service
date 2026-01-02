1b99311db22675cd5dfabac522ec2b007df78ae8
Ujjwal singh <ujzzzwall@gmail.com>
2026-01-02 15:35:01 +0530

    Added gwt craete and verify logic

--- src/services/User-services.js
+++ src/services/User-services.js
@@ -1,4 +1,7 @@
 const UserRepository = require('../repository/User-repository')
+const jwt = require('jsonwebtoken')
+const {JWT_KEY} = require('../config/serverConfig');
+
 
 class UserService{
   constructor(){
@@ -17,9 +20,20 @@ class UserService{
 
   createToken(user){
     try {
-      const result = async this.userRepository
+      const result = jwt.sign(user , JWT_KEY ,{ expiresIn : 30 })
+      return result;
     } catch (error) {
       console.log("Something went wrong in the token creation");
+      throw error;
+    }
+  }
+
+  verifyToken(token){
+    try {
+      const response = jwt.verify(token , JWT_KEY);
+      return response;  
+    } catch (error) {
+      console.log("Something went wrong in the token creation", error);
       throw error;
     }
   }
